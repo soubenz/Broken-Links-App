b53a1e5dd19556545a4e3e25235985947c22d567
soubenz <soubenz94@gmail.com>
2018-06-14 01:48:44 +0200

    add config class

--- config.py
+++ config.py
@@ -1,12 +1,29 @@
 
 
-class Config():
+class Config(object):
   
   
+    
   social_domains = ('buzzfeed.com','facebook.com','vk.com','pinterest.com','twitter.com','instagram.com','tumblr.com')
   isIgnoreSocial = True
+  google_api_key = ''
   
+  def __init__():
+    pass
+    
   
-  def setIgnoreSocial(state)
+ 
+  def setIgnoreSocial(self,state):
     self.isIgnoreSocial = state
     
+  
+  def getIgnoreSocial(self):
+    return self.isIgnoreSocial
+    
+  def getConfigFile(self):
+    config_file = {}
+    return config_file
+    
+    
+  def setApiKey(self, key):
+    self.google_api_key = key
